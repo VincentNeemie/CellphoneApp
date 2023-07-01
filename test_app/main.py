322afa97cc6731de04c15fe8ceb4312ee8d4fc2f
Vincent Neemie <92559302+VincentNeemie@users.noreply.github.com>
2023-07-01 18:43:16 -0300

    Create main.py
    1111

---
+++ test_app/main.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+Builder.load_file(filename='Navek.kv')
+
+class WindowManager(ScreenManager):
+    pass
+
+class Home(Screen):
+    pass
+
+class OtherScreen(Screen):
+    pass
+
+class MyApp(App):
+
+    def build(self):
+        # Create a list of all screens, loop through it and add them to the screen manager
+        screens = [
+            Home(name="home_screen"),
+            OtherScreen(name="other_screen")
+        ]
+
+        self.wm = WindowManager()
+
+        for screen in screens:
+            self.wm.add_widget(screen)
+
+        return self.wm
+
+if __name__ == "__main__":
+    MyApp().run()
